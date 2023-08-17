db83e12e810b0a622c83a17fe9aa4f0b79ef742f
Nkondog Anselme Venceslas <nkanven@yahoo.fr>
2023-08-17 14:41:28 +0100

    Update price.sql

--- SERVER Crypto/price.sql
+++ SERVER Crypto/price.sql
@@ -1,25 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.1.3
--- https://www.phpmyadmin.net/
---
--- Хост: 127.0.0.1
--- Время создания: Сен 20 2022 г., 09:58
--- Версия сервера: 10.4.24-MariaDB
--- Версия PHP: 8.1.4
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
---
--- База данных: `price`
---
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `price`
---
 
 CREATE TABLE `price` (
   `birza` smallint(5) UNSIGNED NOT NULL,
@@ -29,11 +13,6 @@ CREATE TABLE `price` (
   `last_update` int(10) UNSIGNED NOT NULL DEFAULT unix_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `price_history_1d`
---
 
 CREATE TABLE `price_history_1d` (
   `birza` smallint(5) UNSIGNED NOT NULL,
@@ -42,11 +21,6 @@ CREATE TABLE `price_history_1d` (
   `price` decimal(24,14) UNSIGNED NOT NULL DEFAULT 0.00000000000000
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `price_history_1h`
---
 
 CREATE TABLE `price_history_1h` (
   `birza` smallint(5) UNSIGNED NOT NULL,
@@ -56,11 +30,6 @@ CREATE TABLE `price_history_1h` (
   `last_update` int(10) UNSIGNED NOT NULL DEFAULT unix_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `price_history_1m`
---
 
 CREATE TABLE `price_history_1m` (
   `birza` smallint(5) UNSIGNED NOT NULL,
@@ -70,11 +39,6 @@ CREATE TABLE `price_history_1m` (
   `last_update` int(10) UNSIGNED NOT NULL DEFAULT unix_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `price_history_10m`
---
 
 CREATE TABLE `price_history_10m` (
   `birza` smallint(5) UNSIGNED NOT NULL,
@@ -84,46 +48,32 @@ CREATE TABLE `price_history_10m` (
   `last_update` int(10) UNSIGNED NOT NULL DEFAULT unix_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Индексы сохранённых таблиц
---
 
---
--- Индексы таблицы `price`
---
 ALTER TABLE `price`
   ADD PRIMARY KEY (`birza`,`symbol`) USING BTREE;
 
---
--- Индексы таблицы `price_history_1d`
---
+
 ALTER TABLE `price_history_1d`
   ADD PRIMARY KEY (`birza`,`symbol`,`date_day`),
   ADD KEY `birza` (`birza`),
   ADD KEY `symbol` (`symbol`),
   ADD KEY `date_day` (`date_day`);
 
---
--- Индексы таблицы `price_history_1h`
---
+
 ALTER TABLE `price_history_1h`
   ADD PRIMARY KEY (`birza`,`hour`,`symbol`) USING BTREE,
   ADD KEY `symbol` (`symbol`),
   ADD KEY `last_update` (`last_update`),
   ADD KEY `birza` (`birza`);
 
---
--- Индексы таблицы `price_history_1m`
---
+
 ALTER TABLE `price_history_1m`
   ADD PRIMARY KEY (`birza`,`symbol`,`minut`) USING BTREE,
   ADD KEY `last_update` (`last_update`),
   ADD KEY `symbol` (`symbol`),
   ADD KEY `birza` (`birza`);
 
---
--- Индексы таблицы `price_history_10m`
---
+
 ALTER TABLE `price_history_10m`
   ADD PRIMARY KEY (`birza`,`minut`,`symbol`) USING BTREE,
   ADD KEY `last_update` (`last_update`),
